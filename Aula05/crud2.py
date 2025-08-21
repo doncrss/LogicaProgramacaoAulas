2aaa0c2f344e2838e5e03d29ac156542aa50857a
doncrss <cristiano.oliveira2069@gmail.com>
2025-08-21 10:35:17 -0300

    Aula 05

---
+++ Aula05/crud2.py
@@ -0,0 +1,29 @@
+import os
+
+while True:
+    try:
+        arquivo = input('Digite o nome do arquivo (sem extensão):  ').lower().strip()
+
+        with open(f'{arquivo}.txt', 'r', encoding='utf-8') as f:
+            arquivo_aberto = f.read()
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        print(arquivo_aberto)
+
+        while True:
+            prosseguir = input('Deseja abrir outro arquivo? (s/n): ').strip().lower()
+            if prosseguir == 's' or prosseguir == 'n':
+                break
+            else:
+                print('Opção inválida')
+                continue
+        match prosseguir:
+            case 's':
+                continue
+            case 'n':
+                break
+
+    except Exception as e:
+        print(f'Não foi possível ler o arquivo. {e}')
+        continue
